360e6306f86edf8d2a7bf3fe5582c9d1b2a37e1d
tplesko <tea.plesko15@gmail.com>
2025-04-11 09:16:13 +0200

    1st and 2nd tasks

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+res.send('Pozdrav sa Railway servera!');
+});
+app.listen(PORT, () => {
+console.log('Server pokrenut na portu ${PORT}');
+});
